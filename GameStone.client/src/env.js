c020888a0e364b6b2f14958e17897bc44ff8bb71
Chandler Bowman <doctorgrant99@gmail.com>
2023-04-12 15:13:04 -0600

    pushing env

--- GameStone.client/src/env.js
+++ GameStone.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-m7abgi8ld3k2d71d.us.auth0.com'
+export const clientId = 'IgEbeR5frSZ7ctxfhnnfTO6v1pFShOaI'
+export const audience = 'http://sandbox.io'

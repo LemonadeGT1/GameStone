3bb6da4f196d5f72e55853911a276f02713df6fb
Chandler Bowman <doctorgrant99@gmail.com>
2023-04-12 16:32:46 -0600

    added services and models

---
+++ GameStone.client/src/models/GatheringGame.js
@@ -0,0 +1,8 @@
+export class GatheringGame{
+    constructor(data) {
+        this.id = data.id
+        this.gatheringId = data.gatheringId
+        this.gameId = data.gameId
+        this.gameImg = data.gameImg
+    }
+}
---
+++ GameStone.client/src/models/Player.js
@@ -0,0 +1,9 @@
+export class Player {
+    constructor(data) {
+        this.id = data.id
+        this.gatheringId = data.gatheringId
+        this.isInvited = data.isInvited
+        this.accepted = data.accepted
+        this.accountId = data.accountId
+    }
+}

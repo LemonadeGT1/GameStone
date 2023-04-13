f929468bdb04fce9ee61381180472548c838e093
Chandler Bowman <doctorgrant99@gmail.com>
2023-04-13 10:56:10 -0600

    get gatherings

--- GameStone.client/src/pages/HomePage.vue
+++ GameStone.client/src/pages/HomePage.vue
@@ -3,24 +3,24 @@
     <!-- SECTION HERO IMG-->
     <section class="row justify-content-center">
       <div class="col-12">
-        <img class="img-fluid hero-img"
+        <img class="img-fluid position-relative hero-img"
         src="https://images.unsplash.com/photo-1615806647582-53fea422c9b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
         alt="hero-img">
       </div>
-      <div class="col-10 image-text">
-        fgdf
+      <div class="col-10 position-absolute mt-5 image-text">
+        <h1>Welcome to GameStone, the one stop shop for finding players of all kinds to game it out with! Hit one of the above buttons to take a look or click on one of these suggested one!</h1>
       </div>
     </section>
     <!-- SECTION GROUPS -->
     <section class="row justify-content-center p-2">
-      <div class="col-7">
+      <div class="col-7 card elevation-5">
         <h3>Find a Group</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate deleniti veritatis voluptatem illum
           inventore rerum. Unde omnis quasi doloremque, veritatis tempora qui maiores quibusdam ratione officia enim
           aperiam ad laboriosam?</p>
       </div>
       <div class="col-3">
-        <img class="img-fluid group-img"
+        <img class="img-fluid elevation-5 rounded group-img"
           src="https://images.unsplash.com/photo-1643992356285-6eb7c466f505?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80"
           alt="group-img">
       </div>
@@ -28,11 +28,11 @@
     <!-- SECTION GATHERINGS -->
     <section class="row justify-content-center p-2">
       <div class="col-3">
-        <img class="img-fluid gathering-img"
+        <img class="img-fluid rounded elevation-5 gathering-img"
           src="https://images.unsplash.com/photo-1677188010559-0667a1ed33a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1113&q=80"
           alt="gathering-img">
       </div>
-      <div class="col-7">
+      <div class="col-7 card elevation-5 ">
         <h3>Join a Gathering</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quo quae laboriosam architecto. Necessitatibus
           sapiente accusantium dolor dolorem nulla laudantium, reiciendis deserunt, ipsa molestiae odit laborum laboriosam
@@ -43,9 +43,13 @@
 </template>
 
 <script>
+import { AppState } from '../AppState';
+import { computed } from 'vue';
 export default {
   setup() {
-    return {}
+    return {
+      mechanics: computed(() => AppState.gameMechanics)
+    }
   }
 }
 </script>
--- GameStone.client/src/services/GatheringsService.js
+++ GameStone.client/src/services/GatheringsService.js
@@ -1,7 +1,16 @@
+import { AppState } from "../AppState"
+import { Gathering } from "../models/Gathering"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
 
 class GatheringsService {
 
+    async getAllGatherings() {
+        const res = await api.get('api/gatherings')
+        AppState.gatherings = res.data.map(g => new Gathering(g))
+        logger.log('[ALL GATHERINGS]', res.data)
+    }
 }
 
 export const gatheringsService = new GatheringsService()

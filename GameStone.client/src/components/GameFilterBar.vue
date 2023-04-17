bb950788875de607e24df393117bb6f99f3185d9
iangrell <igrell27@outlook.com>
2023-04-17 16:47:26 -0600

    Games search and filter

---
+++ GameStone.client/src/components/GameFilterBar.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+            <h5>Filter By:</h5>
+            <div class="row">
+                <div class="col-1">
+                <div class="dropdown open">
+                    <button class="btn btn-secondary dropdown-toggle rounded-pill" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                                Category
+                            </button>
+                    <div class="dropdown-menu scrollable-menu" aria-labelledby="triggerId">
+                        <button class="dropdown-item" v-for="c in categories" @click="filterByCategory(c.id)">{{ c.name }}</button>
+                        <button class="dropdown-item" href="#">Action</button>
+                        <button class="dropdown-item disabled" href="#">Disabled action</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-1">
+                <div class="dropdown open">
+                    <button class="btn btn-secondary dropdown-toggle rounded-pill" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                                Mechanic
+                            </button>
+                    <div class="dropdown-menu scrollable-menu" aria-labelledby="triggerId">
+                        <button class="dropdown-item" v-for="m in mechanics" @click="filterByMechanic(m.id)">{{ m.name }}</button>
+                        <button class="dropdown-item" href="#">Action</button>
+                        <button class="dropdown-item disabled" href="#">Disabled action</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+</template>
+
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted, ref } from 'vue';
+import { logger } from '../utils/Logger.js';
+import Pop from '../utils/Pop.js';
+import { gamesService } from '../services/GamesService.js';
+export default {
+    setup(){
+
+        const filterCategories = ref('')
+        const categories = AppState.gameCategories
+
+        const filterMechanics = ref('')
+        const mechanics = AppState.gameMechanics
+
+    return { 
+        categories,
+        mechanics,
+
+        Games: computed(() => {
+            if(!filterCategories.value) {
+                return AppState.games
+            } else {return AppState.games.filter(c => c.categories == filterCategories.value)}
+        }),
+
+        async filterByCategory(id) {
+            try {
+                const categoryQuery = id
+                await gamesService.filterByCategory(categoryQuery)
+            } catch (error) {
+                logger.log(error.message)
+                Pop.error(error.message)
+            }
+        },
+
+        async filterByMechanic(id) {
+            try {
+                const mechanicQuery = id
+                await gamesService.filterByMechanic(mechanicQuery)
+            } catch (error) {
+                logger.log(error.message)
+                Pop.error(error.message)
+            }
+        },
+    }
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+.scrollable-menu {
+    height: auto;
+    max-height: 600px;
+    overflow-x: hidden;
+}
+
+</style>

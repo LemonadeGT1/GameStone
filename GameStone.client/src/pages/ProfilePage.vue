df85e8254ae8fea2018bf21e50dd253c373702d0
Isaac Duff <4mustangid@gmail.com>
2023-04-17 13:46:11 -0600

    pushing up profile page and route

---
+++ GameStone.client/src/pages/ProfilePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <h5>Hello from the Profile Page!</h5>
+</template>
+
+
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+
+
+
+
+
+
+
+<style></style>

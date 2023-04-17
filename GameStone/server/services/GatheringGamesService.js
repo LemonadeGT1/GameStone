044d8b18c763feb7635ebf56ca867990716f9953
William Edwards <williamedwards7717@gmail.com>
2023-04-17 10:58:41 -0600

    restrictions on gathering games

--- GameStone/server/services/GatheringGamesService.js
+++ GameStone/server/services/GatheringGamesService.js
@@ -1,9 +1,16 @@
 import { dbContext } from "../db/DbContext"
+import { BadRequest } from "../utils/Errors.js"
+import { gatheringsService } from "./GatheringsService.js"
 
 
 class GatheringGamesService {
     async deleteGatheringGame(gatheringGameId) {
         let gatheringGame = await dbContext.GatheringGames.findById(gatheringGameId)
+
+        if (gatheringGame == null) {
+            throw new BadRequest("Game does not exist")
+        }
+
         await gatheringGame.remove()
     }
 
@@ -14,6 +21,19 @@ class GatheringGamesService {
     }
 
     async addGame(gameData) {
+
+        const foundGame = await dbContext.GatheringGames.find({ gameId: gameData.gameId, gatheringId: gameData.gatheringId })
+
+        if (foundGame[0]) {
+            throw new BadRequest("game is already added")
+        }
+
+        const gathering = await gatheringsService.getGatheringById(gameData.gatheringId)
+
+        if (gathering.isCanceled) {
+            throw new BadRequest("You can't play at a canceled gathering")
+        }
+
         const game = await dbContext.GatheringGames.create(gameData)
         await game.populate("gathering")
         return game

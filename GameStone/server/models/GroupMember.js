b8825e6ea8c5817c8792856d872c512bcedc6381
William Edwards <williamedwards7717@gmail.com>
2023-04-17 10:29:39 -0600

    accountId

--- GameStone/server/models/GroupMember.js
+++ GameStone/server/models/GroupMember.js
@@ -3,12 +3,12 @@ import { Schema } from "mongoose";
 
 export const GroupMemberSchema = new Schema({
     groupId: { type: Schema.Types.ObjectId, required: true, ref: "Group" },
-    profileId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
+    accountId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
     isRestricted: { type: Boolean, required: true, default: false },
 }, { timestamps: true, toJSON: { virtuals: true } })
 
 GroupMemberSchema.virtual("group", {
-    localField: "groupId", 
+    localField: "groupId",
     foreignField: "_id",
     justOne: true,
     ref: "Group"

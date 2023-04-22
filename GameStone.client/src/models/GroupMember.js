9bb6a6c218c86af8ea05d40b8f0903e5c7a32135
Chandler Bowman <doctorgrant99@gmail.com>
2023-04-21 21:16:36 -0600

    group member data

--- GameStone.client/src/models/GroupMember.js
+++ GameStone.client/src/models/GroupMember.js
@@ -6,7 +6,10 @@ export class GroupMember extends Profile {
     constructor(data) {
         super(data.profile)
         this.memberId = data.id
+        // NOTE MAKE SURE TO CLEAR DATABASE 
+        if(data.group) {
         this.group = new Group(data.group)
+        }
+        }
     }
-}
 

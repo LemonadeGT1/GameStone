8db789c50ab34152575a1478c74ab419f82e4cbf
Isaac Duff <4mustangid@gmail.com>
2023-04-21 22:09:16 -0600

    commit

--- GameStone.client/src/pages/AboutPage.vue
+++ GameStone.client/src/pages/AboutPage.vue
@@ -57,11 +57,11 @@
         </div>
         <div class="row">
           <div class="col-4">
-            <h4>Michael Patience <img src="https://s3.amazonaws.com/comicgeeks/characters/avatars/98.jpg?t=1661544547">
+            <h4>Isaac Duff<img src="https://i.insider.com/5e32f2a324306a19834af322?width=700">
             </h4>
           </div>
           <div class="col-8">
-            <p>Spring23 CodeWorks student</p>
+            <p>Spring23 CodeWorks student. A lover of DND, books, video games and his dog Sage. </p>
           </div>
         </div>
         <div class="row">

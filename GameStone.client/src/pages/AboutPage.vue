37c41d7b9d3e9734f1efb8ffe2b17726becc07cd
Isaac Duff <4mustangid@gmail.com>
2023-04-21 22:26:23 -0600

    uppies 2.0

--- GameStone.client/src/pages/AboutPage.vue
+++ GameStone.client/src/pages/AboutPage.vue
@@ -38,8 +38,8 @@
                 src="https://s3.amazonaws.com/comicgeeks/characters/avatars/98.jpg?t=1661544547">
             </div>
             <div class="col-8 d-flex flex-column justify-content-center">
-              <h4 class="p-2">Michael Patience
-              </h4>
+              <h3 class="p-2">Michael Patience
+              </h3>
               <p>Michael is a Spring23 CodeWorks Student. He has enjoyed coding since the days of Apple II's and Commodore
                 64's. Loves Aviation, Board Games, Family, Hiking, and many other activities.</p>
             </div>
@@ -53,7 +53,7 @@
               src="https://cdn.europosters.eu/image/1300/posters/the-big-lebowski-the-dude-i75968.jpg">
           </div>
           <div class="col-8 d-flex flex-column justify-content-center">
-            <h4 class="p-2">Ian Grell</h4>
+            <h3 class="p-2">Ian Grell</h3>
             <p>Also a Spring23 CodeWorks student. The Dude Abides.</p>
           </div>
         </div>
@@ -64,8 +64,8 @@
             <img class="profile-picture" src="https://i.kym-cdn.com/photos/images/facebook/001/274/170/594.png">
           </div>
           <div class="col-8 d-flex flex-column justify-content-center">
-            <h4 class="p-2">Chandler Bowman
-            </h4>
+            <h3 class="p-2">Chandler Bowman
+            </h3>
             <p>We kept calm through the whole project, hopefully it's good enough.WAAAAAAGGHHH</p>
           </div>
         </div>
@@ -76,8 +76,8 @@
             <img class="profile-picture" src="https://i.insider.com/5e32f2a324306a19834af322?width=700">
           </div>
           <div class="col-8 d-flex flex-column justify-content-center">
-            <h4 class="p-2">Isaac Duff
-            </h4>
+            <h3 class="p-2">Isaac Duff
+            </h3>
             <p>Isaac is an active Spring23 CodeWorks student. A lover of DND, books, video games and his dog Sage. </p>
           </div>
         </div>
@@ -89,8 +89,8 @@
               src="https://vignette.wikia.nocookie.net/disney/images/6/64/Kronk_.jpg/revision/latest?cb=20160720194635">
           </div>
           <div class="col-8 d-flex flex-column justify-content-center">
-            <h4 class="p-2">William Edwards
-            </h4>
+            <h3 class="p-2">William Edwards
+            </h3>
             <p>Spring23 CodeWorks student. William enjoys hunting,fishing, video games, golf (even tho he's not very
               good), and watching movies.</p>
           </div>

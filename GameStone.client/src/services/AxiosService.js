c2a6e349349fcd875c5c9b49d2daac8cf985f878
LemonadeGT1 <michael.p.patience@gmail.com>
2023-05-22 12:22:53 -0600

    update client_id for Atlas api

--- GameStone.client/src/services/AxiosService.js
+++ GameStone.client/src/services/AxiosService.js
@@ -8,5 +8,5 @@ export const api = Axios.create({
 export const atlasApi = Axios.create({
   baseURL: 'https://api.boardgameatlas.com/api',
   timeout: 15000,
-  params: { client_id: "JLBr5npPhV", fields: "id,name,description,image_url,min_players,max_players,min_playtime,max_playtime,categories,mechanics,min_age", order_by: "rank", limit: "20" }
+  params: { client_id: "xA1YCEmTwL", fields: "id,name,description,image_url,min_players,max_players,min_playtime,max_playtime,categories,mechanics,min_age", order_by: "rank", limit: "20" }
 })
